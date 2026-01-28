d373125781e90f7afbc4118e9eb118ca0e1e239d
Baber19 <Baber19i@outlook.com>
2026-01-28 12:03:33 +0500

    Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
             ),
             SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
                   style: ButtonStyle(
